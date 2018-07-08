8f0196ef3f4d82b3c341e6ad3f597985844ab740
Vincent <Administrator@PC-20170806QIBH>
2018-07-08 21:01:16 +0800

    ShapeChecker

---
+++ ShapeChecker/ShapeChecker/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ShapeChecker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tslblOpen = new System.Windows.Forms.ToolStripLabel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPoint = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtXPoint = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtYPoint = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDistance = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslblOpen});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(973, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tslblOpen
+            // 
+            this.tslblOpen.Name = "tslblOpen";
+            this.tslblOpen.Size = new System.Drawing.Size(56, 22);
+            this.tslblOpen.Text = "打开图片";
+            this.tslblOpen.Click += new System.EventHandler(this.tslblOpen_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(0, 28);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(664, 612);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(670, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "识别到的光斑坐标： ";
+            // 
+            // lblPoint
+            // 
+            this.lblPoint.AutoSize = true;
+            this.lblPoint.Location = new System.Drawing.Point(782, 150);
+            this.lblPoint.Name = "lblPoint";
+            this.lblPoint.Size = new System.Drawing.Size(41, 12);
+            this.lblPoint.TabIndex = 3;
+            this.lblPoint.Text = "[0, 0]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(670, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "输入鼠标点击X坐标:";
+            // 
+            // txtXPoint
+            // 
+            this.txtXPoint.Location = new System.Drawing.Point(784, 201);
+            this.txtXPoint.Name = "txtXPoint";
+            this.txtXPoint.Size = new System.Drawing.Size(100, 21);
+            this.txtXPoint.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(670, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "输入鼠标点击Y坐标:";
+            // 
+            // txtYPoint
+            // 
+            this.txtYPoint.Location = new System.Drawing.Point(784, 252);
+            this.txtYPoint.Name = "txtYPoint";
+            this.txtYPoint.Size = new System.Drawing.Size(100, 21);
+            this.txtYPoint.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(676, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "鼠标点和光斑距离:";
+            // 
+            // lblDistance
+            // 
+            this.lblDistance.AutoSize = true;
+            this.lblDistance.Location = new System.Drawing.Point(782, 306);
+            this.lblDistance.Name = "lblDistance";
+            this.lblDistance.Size = new System.Drawing.Size(11, 12);
+            this.lblDistance.TabIndex = 9;
+            this.lblDistance.Text = "0";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(886, 301);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "计算";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 642);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblDistance);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtYPoint);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtXPoint);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPoint);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "MainForm";
+            this.Text = "光斑误差计算";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel tslblOpen;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPoint;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtXPoint;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtYPoint;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDistance;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ ShapeChecker/ShapeChecker/Form1.cs
@@ -0,0 +1,233 @@
+﻿using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using AForge.Math.Geometry;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShapeChecker
+{
+    public partial class MainForm : Form
+    {
+        private static double D254 = 2.54d;
+        private float dpiX = 96;
+        /// <summary>
+        /// system dpiX setting, default is 96
+        /// </summary>
+        public float DpiX
+        {
+            get { return dpiX; }
+            set { dpiX = value; }
+        }
+        private PointF pointF;
+        private PointF redPointF;
+
+        public double DigitalMagnification
+        {
+            //1600 -> image size is 1280*960
+            get { return PixelToMillimeter(1600) / 4; }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.pictureBox.Paint += PictureBox_Paint;
+        }
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            //using (Pen p = new Pen(Color.DarkGreen, 1.5f))
+            //{
+            //    if (!pointF.IsEmpty)
+            //    {
+            //        e.Graphics.DrawLine(p, pointF.X, pointF.Y - 10, pointF.X, pointF.Y + 10);
+            //        e.Graphics.DrawLine(p, pointF.X - 10, pointF.Y, pointF.X + 10, pointF.Y);
+            //    }
+            //}
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            LoadDemo(string.Format("{0}\\{1}",  System.Environment.CurrentDirectory,"Images\\1.png"));
+        }
+
+        // Load one of the embedded demo image
+        private void LoadDemo(string embeddedFileName)
+        {
+            // load arrow bitmap
+            Bitmap image = new Bitmap(embeddedFileName);
+            ProcessImage(image);
+        }
+
+        private void tslblOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog.InitialDirectory = string.Format("{0}\\Images", System.Environment.CurrentDirectory);
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ProcessImage((Bitmap)Bitmap.FromFile(openFileDialog.FileName));
+                }
+                catch
+                {
+                    MessageBox.Show("加载所选图片失败。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Process image
+        private void ProcessImage(Bitmap bitmap)
+        {
+            // lock image
+            BitmapData bitmapData = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            // step 1 - turn background to black
+            ColorFiltering colorFilter = new ColorFiltering();
+
+            colorFilter.Red = new IntRange(0, 254);
+            colorFilter.Green = new IntRange(0, 254);
+            colorFilter.Blue = new IntRange(0, 254);
+            colorFilter.FillOutsideRange = false;
+
+            colorFilter.ApplyInPlace(bitmapData);
+
+            // step 2 - locating objects
+            BlobCounter blobCounter = new BlobCounter();
+
+            blobCounter.FilterBlobs = true;
+            blobCounter.MinHeight = 5;
+            blobCounter.MinWidth = 5;
+
+            blobCounter.ProcessImage(bitmapData);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            bitmap.UnlockBits(bitmapData);
+
+            // step 3 - check objects' type and highlight
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+
+            Graphics g = Graphics.FromImage(bitmap);
+            Pen yellowPen = new Pen(Color.Yellow, 2); // circles
+            Pen redPen = new Pen(Color.Red, 2);       // quadrilateral
+            Pen brownPen = new Pen(Color.Brown, 2);   // quadrilateral with known sub-type
+            Pen greenPen = new Pen(Color.Green, 2);   // known triangle
+            Pen bluePen = new Pen(Color.Blue, 2);     // triangle
+
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+
+                DoublePoint center;
+                double radius;
+
+                // is circle ?
+                if (shapeChecker.IsCircle(edgePoints, out center, out radius))
+                {
+                    g.DrawEllipse(yellowPen,
+                        (float)(center.X - radius), (float)(center.Y - radius),
+                        (float)(radius * 2), (float)(radius * 2));
+                    redPointF = new PointF((float)center.X, (float)center.Y);
+                    lblPoint.Text = string.Format("[{0}, {1}]", center.X, center.Y);
+                }
+                else
+                {
+                    List<IntPoint> corners;
+
+                    // is triangle or quadrilateral
+                    if (shapeChecker.IsConvexPolygon(edgePoints, out corners))
+                    {
+                        // get sub-type
+                        PolygonSubType subType = shapeChecker.CheckPolygonSubType(corners);
+
+                        Pen pen;
+
+                        if (subType == PolygonSubType.Unknown)
+                        {
+                            pen = (corners.Count == 4) ? redPen : bluePen;
+                        }
+                        else
+                        {
+                            pen = (corners.Count == 4) ? brownPen : greenPen;
+                        }
+
+                        g.DrawPolygon(pen, ToPointsArray(corners));
+                    }
+                }
+            }
+
+            yellowPen.Dispose();
+            redPen.Dispose();
+            greenPen.Dispose();
+            bluePen.Dispose();
+            brownPen.Dispose();
+            g.Dispose();
+
+            // put new image to clipboard
+            Clipboard.SetDataObject(bitmap);
+            // and to picture box
+            pictureBox.Image = bitmap;
+        }
+
+        // Conver list of AForge.NET's points to array of .NET points
+        private Point[] ToPointsArray(List<IntPoint> points)
+        {
+            Point[] array = new Point[points.Count];
+
+            for (int i = 0, n = points.Count; i < n; i++)
+            {
+                array[i] = new Point(points[i].X, points[i].Y);
+            }
+
+            return array;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var px = double.Parse(txtXPoint.Text);
+                var py = double.Parse(txtYPoint.Text);
+                pointF = new PointF((float)px, (float)py);
+                var x = PixelToMicroscope(System.Math.Abs(pointF.X - redPointF.X));
+                var y = PixelToMicroscope(System.Math.Abs(pointF.Y - redPointF.Y));
+                string cir = string.Format("{0:F2} {1}", Math.Sqrt(x * x + y * y), "um");
+                this.lblDistance.Text = cir;
+                this.pictureBox.Invalidate();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private double PixelToMicroscope(double pixel)
+        {
+            return PixelToMicron(pixel) / DigitalMagnification;
+        }
+
+        private double PixelToMicron(double pixel)
+        {
+            return PixelToMillimeter(pixel) * 1000;
+        }
+
+        private double PixelToCentimeter(double pixel)
+        {
+            return pixel * D254 / DpiX;
+        }
+
+        private double PixelToMillimeter(double pixel)
+        {
+            return PixelToCentimeter(pixel) * 10;
+        }
+    }
+}
